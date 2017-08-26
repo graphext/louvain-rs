f3fb20cfb1eb1f58c13629674c5088c540b28b97
Juan Morales <crispamares@gmail.com>
2017-08-26 15:22:42 +0200

    Add some time meassures

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-#![feature(proc_macro)]
-
-#[macro_use]
-extern crate serde_derive;
 extern crate petgraph;
-extern crate serde_json;
-extern crate louvain;
 extern crate chrono;
+extern crate louvain;
 
 use std::env;
 use std::collections::{HashMap};
@@ -16,27 +11,29 @@ use petgraph::graph::{NodeIndex};
 use louvain::{Modularity};
 use louvain::io::{Node, NodeID, Edge, Community, read_json_file, write_json_file};
 
-
-fn main() {
+fn main() { 
+    let mut start_time = chrono::Utc::now();
     // println!("Reading files...");
     // let nodes: Vec<Node> = read_json_file("../miserables_nodes.json");
     // let edges: Vec<Edge> = read_json_file("../miserables_links.json");
     let args : Vec<String> = env::args().collect();
     let nodes: Vec<Node> = read_json_file(&args[1]);
+    println!("Read nodes {:?}", chrono::Utc::now().signed_duration_since(start_time));
+    start_time = chrono::Utc::now();
     let edges: Vec<Edge> = read_json_file(&args[2]);
-
+    println!("Read links {:?}", chrono::Utc::now().signed_duration_since(start_time));
 
     let resolution : f64 = match args.get(3) {
         Some(res) => res.parse().expect("Resolution should be a float. Default = 1.0"),
         None => 1.0
     };
-
     //compute_louvain( &nodes, &edges);
-    // println!("Nodes: {}", nodes.len());
-    // println!("Edges: {}", edges.len());
+    println!("Nodes: {}", nodes.len());
+    println!("Edges: {}", edges.len());
 
     //let graph = Graph {nodes: nodes, edges: edges};
 
+    start_time = chrono::Utc::now();
     let mut inv_map: HashMap<NodeID, NodeIndex> = HashMap::new();
     let mut graph = Graph::new_undirected();
     for ref node in &nodes {
@@ -53,12 +50,15 @@ fn main() {
             *inv_map.get(&edge.target).unwrap(),
             edge.weight);
     }
+    println!("Construct the graph computed in {:?}", chrono::Utc::now().signed_duration_since(start_time));
 
+
+    start_time = chrono::Utc::now();
     let mut modularity = Modularity::new(resolution);
-    let start_time = chrono::UTC::now();
     let results = modularity.execute(& graph);
-    println!("Louvain Clusters computed in {:?}", chrono::UTC::now() - start_time);
+    println!("Louvain Clusters computed in {:?}", chrono::Utc::now().signed_duration_since(start_time));
 
+    start_time = chrono::Utc::now();
     let num_of_communities = modularity.communityByNode.iter().max().unwrap_or(&0) + 2; // Parent Community included
     println!("Number of Clusters: {:?} -  with resolution {:?}", num_of_communities-1, resolution);
     println!("Final Modularity: {:?}", results);
@@ -80,4 +80,5 @@ fn main() {
     // println!("{:?}", communities);
 
     write_json_file("communities.json", &communities);
+    println!("Written communities {:?}", chrono::Utc::now().signed_duration_since(start_time));
 }
