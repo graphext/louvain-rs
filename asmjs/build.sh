dc672ee1fa8c9f67d11a5b124234cc9ca5ff4a66
Juan Morales <crispamares@gmail.com>
2018-09-29 14:20:22 +0200

    Remove asmjs

--- asmjs/build.sh
+++
@@ -1,5 +0,0 @@
-## The total memory is fixed... must be multiple of 16MB
-EMMAKEN_CFLAGS="-s TOTAL_MEMORY=167772160" cargo web start --host 0.0.0.0 --release
-
-## This flag truns down some optimizations
-# EMMAKEN_CFLAGS="-s ALLOW_MEMORY_GROWTH=1" cargo web start --host 0.0.0.0 --release           
